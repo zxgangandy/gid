051ee1a62d60124942f47dece42d2745a04db6b1
andy zhao <286505491@qq.com>
2022-03-09 19:06:42 +0800

    add comments;

--- config/gid_config.go
+++ config/gid_config.go
@@ -2,35 +2,46 @@ package config
 
 import "gorm.io/gorm"
 
+// uid configure interface
 type UidConfig interface {
+	//get db handler
 	GetDB() *gorm.DB
 
+	//get port
 	GetPort() string
 
+	//get time bits
 	GetTimeBits() uint32
 
+	//get worker bits
 	GetWorkerBits() uint32
 
+	//get sequence bits
 	GetSeqBits() uint32
 
+	//get seconds of the epoch time
 	GetEpochSeconds() int64
 
+	//get max backward seconds
 	GetMaxBackwardSeconds() int64
 
+	//get enable backward status
 	EnableBackward() bool
 }
 
+// the default uid configure
 type DefaultUidConfig struct {
-	DB                 *gorm.DB
-	Port               string // App port
-	TimeBits           uint32 // time bits
-	WorkerBits         uint32 // worker bits
-	SeqBits            uint32 // sequence bits
-	EpochSeconds       int64  // epoch seconds
-	MaxBackwardSeconds int64  // max backward seconds
-	IsEnableBackward   bool   // enable clock backward
+	DB                 *gorm.DB // db handler
+	Port               string   // app port
+	TimeBits           uint32   // time bits
+	WorkerBits         uint32   // worker bits
+	SeqBits            uint32   // sequence bits
+	EpochSeconds       int64    // epoch seconds
+	MaxBackwardSeconds int64    // max backward seconds
+	IsEnableBackward   bool     // enable clock backward
 }
 
+//create a default uid configure instance
 func New(db *gorm.DB, port string) *DefaultUidConfig {
 	return &DefaultUidConfig{
 		DB:                 db,
@@ -44,34 +55,42 @@ func New(db *gorm.DB, port string) *DefaultUidConfig {
 	}
 }
 
+//get db handler
 func (c *DefaultUidConfig) GetDB() *gorm.DB {
 	return c.DB
 }
 
+//get app port
 func (c *DefaultUidConfig) GetPort() string {
 	return c.Port
 }
 
+//get time bits
 func (c *DefaultUidConfig) GetTimeBits() uint32 {
 	return c.TimeBits
 }
 
+//get worker bits
 func (c *DefaultUidConfig) GetWorkerBits() uint32 {
 	return c.WorkerBits
 }
 
+//get sequence bits
 func (c *DefaultUidConfig) GetSeqBits() uint32 {
 	return c.SeqBits
 }
 
+//get seconds of the epoch time
 func (c *DefaultUidConfig) GetEpochSeconds() int64 {
 	return c.EpochSeconds
 }
 
+//get the seconds of the max backward
 func (c *DefaultUidConfig) GetMaxBackwardSeconds() int64 {
 	return c.MaxBackwardSeconds
 }
 
+//get enable backward status
 func (c *DefaultUidConfig) EnableBackward() bool {
 	return c.IsEnableBackward
 }
