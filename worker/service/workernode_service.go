b5aa417d7a4ee8e872cec697f1fb309f94279983
andy zhao <286505491@qq.com>
2022-03-10 11:32:34 +0800

    add comments;

--- worker/service/workernode_service.go
+++ worker/service/workernode_service.go
@@ -6,15 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
+//worker node service interface
 type IWorkerNodeService interface {
 	GetByHostname(HostName string) (*model.WorkerNode, error)
 	Save(node *model.WorkerNode) (bool, error)
 }
 
+//worker node service
 type WorkerNodeService struct {
 	workerNodeDao dao.IWorkerNodeDao
 }
 
+//create worker node service instance
 func NewWorkerNodeService(db *gorm.DB) *WorkerNodeService {
 	workerNodeDao := dao.NewWorkerNodeDao(db)
 	return &WorkerNodeService{
@@ -22,10 +25,12 @@ func NewWorkerNodeService(db *gorm.DB) *WorkerNodeService {
 	}
 }
 
+//get worker node by hostname
 func (w *WorkerNodeService) GetByHostname(HostName string) (*model.WorkerNode, error) {
 	return w.workerNodeDao.GetByHostname(HostName)
 }
 
+//save  worker node
 func (w *WorkerNodeService) Save(node *model.WorkerNode) (bool, error) {
 	return w.workerNodeDao.Save(node)
 }
