6d896508915bc71f969596d976d3d148afed9c71
andy zhao <286505491@qq.com>
2022-07-14 22:33:01 +0800

    update tests;

--- tests/gid_test.go
+++ tests/gid_test.go
@@ -8,18 +8,39 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 	"testing"
+	"time"
 )
 
-func TestGenId(t *testing.T) {
+func TestDefaultGenId(t *testing.T) {
 	dsn := "root:root@tcp(localhost:3306)/jingwei-exchange?charset=utf8&parseTime=True&loc=Local"
 	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
 	c := config.New(db, "8000")
-	fmt.Println(gid.New(c).GetUID())
 
-	c.WorkerBits = 20
-	c.SeqBits = 13
+	start := time.Now().UnixMilli()
+	gen := gid.New(c)
+	for i := 0; i < 10000; i++ {
+		_ = gen.GetUID()
+	}
+	end := time.Now().UnixMilli()
+	fmt.Println(end - start)
+}
 
-	fmt.Println(gid.New(c).GetUID())
+func TestCustomGenId(t *testing.T) {
+	dsn := "root:root@tcp(localhost:3306)/jingwei-exchange?charset=utf8&parseTime=True&loc=Local"
+	db, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+		SingularTable: true,
+	}})
+	c := config.New(db, "8000")
+	c.WorkerBits = 10
+	c.SeqBits = 23
+
+	start := time.Now().UnixMilli()
+	gen := gid.New(c)
+	for i := 0; i < 10000000; i++ {
+		_ = gen.GetUID()
+	}
+	end := time.Now().UnixMilli()
+	fmt.Println(end - start)
 }
